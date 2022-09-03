43bc182a4485a89fb4f9b3c406b3923475b03368
keigo-h <keigo-h@weseek.co.jp>
2022-09-03 14:20:01 +0900

    setup environment(first commit)

---
+++ README.md
@@ -0,0 +1,7 @@
+# 開発準備
+
+- run `docker-compose run --rm app sh`
+
+# 終了時
+
+run `exit` or Ctrl + c
---
+++ app/server/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', async (req, res) => {
+  return res.status(200).send({
+    message: 'Hello World!',
+  });
+});
+
+app.listen(PORT, () => {
+  console.log('Listening on PORT: 3000');
+});
