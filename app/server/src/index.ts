cff1276c3447b24545db79dbbe293a99f0223b15
keigo-h <keigo-h@weseek.co.jp>
2022-09-14 20:52:13 +0900

    create database and establish database connection

--- app/server/src/index.ts
+++ app/server/src/index.ts
@@ -1,24 +1,26 @@
 import express from 'express';
+import { existsSync } from 'fs';
 import mysql from 'mysql2';
 // Todo: build server using graphQL
 // import { buildSchema } from 'graphql';
 // import { graphqlHTTP } from 'express-graphql';
 
-const connection = mysql.createConnection({
+const app = express();
+const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
+  database: 'gent',
   password: '158281872keigo',
-  database: 'test',
+  connectionLimit: 10,
 });
 
-const app = express();
-connection.connect((error) => {
-  if (error) {
-    console.log(`Error occured cause: ${error}`);
-    return;
-  }
-  console.log(`Connection established! id: ${connection.threadId}`);
+// If databese gent is not exist, excute <CREATE DATABASE IF NOT EXISTS `gent`;> next, <use `gent`;> command!
+pool.getConnection((err, connection) => {
+  if (err) throw err;
+
+  console.log('connection established!');
 });
+
 const PORT = 5000;
 
 app.get('/', async (req, res) => {
