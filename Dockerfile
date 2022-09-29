3f3c2b8bbda64cb620cf6ec705672fc01435e8af
keigo-h <keigo-h@weseek.co.jp>
2022-09-29 18:29:26 +0900

    migrate db server and nodejs server

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM node:17.6.0
+
+WORKDIR /app
+
+RUN npm install
---
+++ app/server/src/prisma/migrations/20220929085711_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Story" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Story_pkey" PRIMARY KEY ("id")
+);
