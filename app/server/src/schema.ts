0a565a4fccaf859852ba12c68d9aa6fce8171e20
keigo-h <keigo-h@weseek.co.jp>
2022-09-30 06:16:09 +0900

    make task schema

--- app/server/src/schema.ts
+++ app/server/src/schema.ts
@@ -3,22 +3,18 @@ import { gql } from 'apollo-server';
 export const typeDefs = gql`
   type Query {
     getStories: [Story]
+    getTasks(status: String): [Task]
   }
 
   type Mutation {
-    createStory(name: String!): CreateStoryResponse
-    renameStory(targetId: ID!, newName: String!): RenameStoryResponse
+    createStory(name: String!): StoryResponse
+    renameStory(targetId: ID!, newName: String!): StoryResponse
     deleteStory(targetId: ID!): DeleteStoryResponse
+
+    createTask(storyid: String, taskName: String): TaskResponse
   }
 
-  type CreateStoryResponse {
-    code: Int!
-    success: Boolean!
-    message: String!
-    story: Story
-  }
-
-  type RenameStoryResponse {
+  type StoryResponse {
     code: Int!
     success: Boolean!
     message: String!
@@ -34,5 +30,19 @@ export const typeDefs = gql`
   type Story {
     id: ID!
     name: String!
+  }
+
+  type TaskResponse {
+    code: Int!
+    success: Boolean!
+    message: String!
+    task: Task
+  }
+
+  type Task {
+    id: ID!
+    name: String!
+    status: String!
+    story_id: ID!
   }
 `;
