e44a18105bc701e7a684b4670296d47542fe58bb
keigo-h <keigo-h@weseek.co.jp>
2022-10-05 10:00:36 +0900

    refactor story crud operation

--- app/server/src/resolver.ts
+++ app/server/src/resolver.ts
@@ -1,8 +1,7 @@
-import { CreateStoryArgsType } from './types/CreateStoryArgsType';
 import {
   getStories,
   createStory,
-  changeStoryName,
+  renameStory,
   deleteStory,
 } from './service/story';
 
@@ -13,8 +12,6 @@ import {
   updateTaskStatus,
   deleteTask,
 } from './service/task';
-import { RenameStoryArgsType } from './types/RenameStoryArgsType';
-import { DeleteStoryArgsType } from './types/DeleteStoryArgsType';
 
 export const resolvers = {
   Query: {
@@ -22,62 +19,9 @@ export const resolvers = {
     getTasks,
   },
   Mutation: {
-    createStory: async (_: null, { name }: CreateStoryArgsType) => {
-      try {
-        const story = await createStory(name);
-        return {
-          code: 201,
-          success: true,
-          message: 'New story was successfully created!',
-          story,
-        };
-      } catch (error) {
-        console.log(error);
-        return {
-          code: 500,
-          success: false,
-          message: 'Something went wrong on creating new story...',
-        };
-      }
-    },
-    renameStory: async (
-      _: null,
-      { targetId, newName }: RenameStoryArgsType
-    ) => {
-      try {
-        const newStory = await changeStoryName(targetId, newName);
-        return {
-          code: 201,
-          success: true,
-          message: 'The Story was successfully renamed!',
-          story: newStory,
-        };
-      } catch (error) {
-        console.log(error);
-        return {
-          code: 500,
-          success: false,
-          message: 'Something went wrong on updating story...',
-        };
-      }
-    },
-    deleteStory: async (_: null, { targetId }: DeleteStoryArgsType) => {
-      try {
-        await deleteStory(targetId);
-        return {
-          code: 204,
-          success: true,
-          message: 'The story was successfully deleted!',
-        };
-      } catch (error) {
-        console.log(error);
-        return {
-          code: 500,
-          success: false,
-          message: 'Something went wrong on deleting story...',
-        };
-      }
-    },
+    createStory,
+    renameStory,
+    deleteStory,
     createTask,
     renameTask,
     updateTaskStatus,
--- app/server/src/service/story.ts
+++ app/server/src/service/story.ts
@@ -1,37 +1,92 @@
+import { CreateStoryArgsType } from '../types/CreateStoryArgsType';
+import { RenameStoryArgsType } from '../types/RenameStoryArgsType';
+import { DeleteStoryArgsType } from '../types/DeleteStoryArgsType';
 import { prisma } from './prismaClient';
 
 export const getStories = async () => {
   return await prisma.story.findMany();
 };
 // Todo: add error handling and use these functions directly
-export const createStory = async (name: string) => {
-  const id = new Date().getTime().toString();
-  const newStory = await prisma.story.create({
-    data: {
-      id,
-      name,
-    },
-  });
-  return newStory;
+export const createStory = async (_: null, { name }: CreateStoryArgsType) => {
+  try {
+    const id = new Date().getTime().toString();
+    const newStory = await prisma.story.create({
+      data: {
+        id,
+        name,
+      },
+    });
+    return {
+      code: 201,
+      success: true,
+      message: 'New story was successfully created!',
+      newStory,
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      code: 500,
+      success: false,
+      message: error,
+    };
+  }
 };
 
-export const changeStoryName = async (id: string, newName: string) => {
-  const updatedStory = await prisma.story.update({
-    where: {
-      id,
-    },
-    data: {
-      name: newName,
-    },
-  });
-  return updatedStory;
+// export const changeStoryName = async (id: string, newName: string) => {
+
+//   return updatedStory;
+// };
+
+export const renameStory = async (
+  _: null,
+  { targetId, newName }: RenameStoryArgsType
+) => {
+  try {
+    const renamedStory = await prisma.story.update({
+      where: {
+        id: targetId,
+      },
+      data: {
+        name: newName,
+      },
+    });
+    return {
+      code: 201,
+      success: true,
+      message: 'The Story was successfully renamed!',
+      story: renamedStory,
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      code: 500,
+      success: false,
+      message: error,
+    };
+  }
 };
 
-export const deleteStory = async (id: string) => {
-  const deleteStory = await prisma.story.delete({
-    where: {
-      id,
-    },
-  });
-  return deleteStory;
+export const deleteStory = async (
+  _: null,
+  { targetId }: DeleteStoryArgsType
+) => {
+  try {
+    await prisma.story.delete({
+      where: {
+        id: targetId,
+      },
+    });
+    return {
+      code: 204,
+      success: true,
+      message: 'The story was successfully deleted!',
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      code: 500,
+      success: false,
+      message: 'Something went wrong on deleting story...',
+    };
+  }
 };
