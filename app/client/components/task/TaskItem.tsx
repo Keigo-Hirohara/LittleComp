1e29b2b8303fe214343e3140dad5d4323a3516ff
keigo-h <keigo-h@weseek.co.jp>
2022-11-16 08:05:35 +0900

    add hover styling to taskitem

--- app/client/components/task/TaskItem.tsx
+++ app/client/components/task/TaskItem.tsx
@@ -4,7 +4,7 @@ import { editTaskModalState } from '../../context/taskState';
 
 const TaskItem = (props: Task): JSX.Element => {
   return (
-    <div className="relative flex items-center w-80 h-80 bg-green1 rounded-3xl ml-10 mb-10">
+    <div className="relative group flex items-center w-80 h-80 bg-green1 rounded-3xl ml-10 mb-10">
       <p className=" text-white text-xs mx-5 break-words line-clamp-4">
         {props.name}
       </p>
@@ -18,7 +18,7 @@ const TaskItem = (props: Task): JSX.Element => {
             taskId: props.id,
           })
         }
-        className=" w-13 h-13 absolute bottom-5 right-5 text-white"
+        className="w-13 h-13 absolute bottom-5 right-5 text-white invisible group-hover:visible"
       />
     </div>
   );
