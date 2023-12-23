3071e8231b21bf613b3aaf49768ec802a01a3309
Keigo-Hirohara <hirokei.camel@gmail.com>
2023-12-23 15:16:22 +0900

    トークンが設定されていない場合のcannot read properties of undefinedエラーを修正

--- app/server/src/index.ts
+++ app/server/src/index.ts
@@ -3,7 +3,7 @@ import { typeDefs } from './schema';
 import { resolvers } from './resolver';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
-import { Context } from './types/Context';
+import { Context, Jwt } from './types/Context';
 
 // Todo: Federate each service into subqueries locatedError...
 if (process.env.NODE_ENV !== 'production') {
@@ -11,6 +11,16 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const authContext = async ({ req }: any): Promise<Context> => {
+  const jwtInfo: Jwt = {
+    secret: process.env.JWT_SECRET || 'secret',
+    expiresIn: process.env.JWT_EXPIRES_IN || '1d',
+  };
+  if (!req.headers.authorization) {
+    return {
+      jwt: jwtInfo,
+      verified: null,
+    };
+  }
   const token = req.headers.authorization.split(' ');
   let verified = null;
   const secret = process.env.JWT_SECRET || '';
@@ -21,10 +31,7 @@ const authContext = async ({ req }: any): Promise<Context> => {
     console.log(error.message);
   }
   return {
-    jwt: {
-      secret: process.env.JWT_SECRET || 'secret',
-      expiresIn: process.env.JWT_EXPIRES_IN || '1d',
-    },
+    jwt: jwtInfo,
     verified,
   };
 };
